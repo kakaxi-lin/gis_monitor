53c58abf5c9581ab8fddb0ebed325371a9432eb8
kiki1 <7405911@qq.com>
2017-01-09 11:51:57 +0800

    use new map

--- WebRoot/js/map.js
+++ WebRoot/js/map.js
@@ -5,13 +5,22 @@
     var spatialReference;
     var pointLayer=null;
     var polylineLayer=null;
+    //点标记集合
     var pointArray=[];
-    pointArray.push("12971560.146692835,4841705.405986843");
-    pointArray.push("12972480.898534339,4841975.2815265935");
-    pointArray.push("12973281.000134543,4841597.455770941");
-    pointArray.push("12972626.94882644,4841178.35493274");
-    pointArray.push("12971760.172092887,4841292.655161341");
-    pointArray.push("12971472.569434348,4841721.545602454");
+    pointArray.push("12957094.32702747,4903416.118362501");
+    pointArray.push("12964370.383246249,4905797.373125011");
+    pointArray.push("12969873.727586271,4902992.784182499");
+    pointArray.push("12965799.136103755,4901193.613917492");
+    pointArray.push("12962121.42041499,4901590.489711244");
+    pointArray.push("12957729.328297472,4903230.9096587505");
+    //线标记集合
+    var lineArray=[];
+    lineArray.push("12958840.580519976,4903469.035135001");
+    lineArray.push("12962994.547161244,4905347.580558759");
+    lineArray.push("12969265.184702517,4901590.489711244");
+    lineArray.push("12966883.92994001,4899764.861059986");
+    lineArray.push("12962068.50364249,4899288.610107484");
+    lineArray.push("12969582.68533752,4908125.71111502");
     var zoom;
     var pointGraphic;
     //定义图片的大小 30x40
@@ -21,7 +30,8 @@
     	map = new esri.Map("map", {logo: false});
     	spatialReference=new esri.SpatialReference({wkid:102113});
     	//先添加的图层在最下面
-        var baseMap = new esri.layers.ArcGISTiledMapServiceLayer("http://130.10.7.207:8399/rest/services/chaoyang_SDE_1211/MapServer");
+//        var baseMap = new esri.layers.ArcGISTiledMapServiceLayer("http://130.10.7.207:8399/rest/services/chaoyang_SDE_1211/MapServer");
+        var baseMap = new esri.layers.ArcGISTiledMapServiceLayer("http://130.10.7.207:8399/rest/services/lgycsh_anno_20170105/MapServer");
         map.addLayer(baseMap);
         //添加线图层
         polylineLayer=new esri.layers.GraphicsLayer({id:"polylineLayer"});
@@ -32,19 +42,16 @@
         map.setLevel(4);
         dojo.connect(map, "onLoad",function(){
 			zoom=map.getZoom();
+console.log(zoom);
 			imgSizeX=10+5*zoom;
 			imgSizeY=20+5*zoom;
         });
         dojo.connect(map, "onClick",function(e){
-			console.log(e.mapPoint.x+","+e.mapPoint.y);
+console.log(e.mapPoint.x+","+e.mapPoint.y);
         });
         dojo.connect(map,"onZoomEnd",function(e){
-            //console.log(map.getZoom());
-            //console.log(map.getLevel());
-        	var points=pointLayer.graphics;
-        	if(points.length==0){
-              return;
-            }
+console.log(map.getZoom());
+console.log(map.getLevel());
         	zoom=map.getZoom();
         	imgSizeX=10+5*zoom;
 			imgSizeY=20+5*zoom;
@@ -61,18 +68,18 @@
     //显示点标记
     function showPoint(){
     	clearMap();
-        if(pointArray.length==0){
+        if(lineArray.length==0){
           alert("没有点标记!");
           return;
         }
-        for(var i in pointArray){
-        	var point = new esri.geometry.Point(pointArray[i].split(",")[0],pointArray[i].split(",")[1],spatialReference)
+        for(var i in lineArray){
+        	var point = new esri.geometry.Point(lineArray[i].split(",")[0],lineArray[i].split(",")[1],spatialReference)
             var symbol = new esri.symbol.PictureMarkerSymbol("img/1.png",imgSizeX,imgSizeY);
             symbol.setOffset(0, imgSizeY/2);
             var graphic = new esri.Graphic(point,symbol);
             pointLayer.add(graphic);
         }
-		console.log(pointLayer.graphics);
+//		console.log(pointLayer.graphics);
     }
     //显示开始结束点标记
     function showP_Start_End(){
@@ -97,7 +104,7 @@
             var graphic = new esri.Graphic(point,symbol);
             pointLayer.add(graphic);
         }
-		console.log(pointLayer.graphics);
+//		console.log(pointLayer.graphics);
     }
     //显示点线标记
     function showPolyline(){
@@ -113,7 +120,7 @@
 				   new esri.Color([255,0,0]), 3);
 	    var graphic=new esri.Graphic(polyline,symbol);
 		polylineLayer.add(graphic);
-		console.log(polylineLayer.graphics);
+//		console.log(polylineLayer.graphics);
     }
     function clearMap(){
     	pointLayer.clear();
